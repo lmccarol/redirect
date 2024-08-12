3ee9e5024bdd8cd2a85c41ddc6a29eb55e642c78
lmccarol <149507340+lmccarol@users.noreply.github.com>
2024-08-12 16:18:59 -0400

    Added files via upload

---
+++ resources/lang/en/index.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'Titlemsg' => 'Your Account is Inactive.',
+    'msgbox' => 'Your account is not activated. Please visit our portal to make a payment in order to activate your account.',
+    'portal' => 'Portal'
+
+];
---
+++ resources/lang/fr/index.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'Titlemsg' => 'Votre compte est inactif.',
+    'msgbox' => 'Votre compte n\'est pas activé. Veuillez visiter notre portail pour effectuer un paiement afin d\'activer votre compte.',
+    'portal' => 'Portail'
+    
+];
